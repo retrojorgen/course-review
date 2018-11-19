d033e86b639f21610fb29a4b73a14097633fa5ba
Jørgen Jacobsen <jorgeja@gmail.com>
2018-11-19 22:17:55 +0100

    removed .env

--- db.js
+++ db.js
@@ -1,36 +1,85 @@
-const mongoose = require("mongoose");
-console.log("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@ds123753.mlab.com:23753/kode24-julebase");
-mongoose.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@ds123753.mlab.com:23753/kode24-julebase");
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@ds021036.mlab.com:21036/testdatabase")
 
 
-const competitionsSchema = new mongoose.Schema({
-  type: {type: String},
-  name: {type: String},
-  namespace: {type: String},
-  participants: [
-    {
-      email: {type: String},
-      name: {type: String},
-      submitted: { type: Date, default: Date.now }
+const coursesSchema = new mongoose.Schema({
+  name: {type: String}
+})
+
+const reviewsSchema = new mongoose.Schema({
+  description: {type: String},
+  stars: {type: Number, min: 1, max: 6},
+  submitted: { type: Date, default: Date.now },
+  userId: mongoose.Schema.Types.ObjectId,
+  courseId: mongoose.Schema.Types.ObjectId
+})
+
+const usersSchema = new mongoose.Schema({
+  username: {type: String, unique: true},
+  password: {type: String}
+})
+
+const Courses = mongoose.model("courses", coursesSchema)
+const Reviews = mongoose.model("reviews", reviewsSchema)
+const Users = mongoose.model("users", usersSchema)
+
+function findAllCourses (callback) {
+  Courses.find({}, (error, courses) => {
+    if(!error)
+      callback(courses);
+    else
+      callback(false);
+  })
+}
+
+function addCourse (course, callback) {
+  let newCourse = new Courses(course);
+  newCourse.save(err, () => {
+    if(!err)
+      callback(newCourse);
+    else
+      calback(false);
+  });
+}
+
+function findAllReviews (callback) {
+  courses.find({}, (error, courses) => {
+    if(!error)
+      callback(courses);
+    else
+      callback(false);
+  })
+}
+
+function addReview (review, courseId, userId, callback) {
+  let newReview = new Reviews(review);
+  newReview.courseId = mongoose.Types.ObjectId(courseId);
+  newReview.userId = mongoose.Types.ObjectId(userId);
+  newReview.save(err, () => {
+    if(!err)
+      callback(newCourse);
+    else
+      calback(false);
+  });
+}
+
+
+function findUserByUsernameAndPassword (username, password, callback) {
+  Users.findOne({username: username, password: password}, (error, user) => {
+    if(!error) {
+      callback(user);
     }
-  ]})
-
-const Competitions = mongoose.model("competitions", competitionsSchema)
-
-async function findAllCompetitions () {
-  try {
-    let competitions = await Competitions.find({});
-    console.log(competitions);
-    return competitions;
-  }
-  catch (error) {
-    console.log(error);
-    return false;
-  }
-
-  
+    else {
+      callback(false);
+    }
+      
+  })
 }
 
 module.exports = {
-  findAllCompetitions: findAllCompetitions
+  findAllCourses: findAllCourses,
+  findAllReviews: findAllReviews,
+  addCourse: addCourse,
+  addReview: addReview,
+  findUserByUsernameAndPassword: findUserByUsernameAndPassword
 }
--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 require('dotenv').config()
 
-console.log(process.env);
 const app = require('express')()
+const bodyParser = require('body-parser')
 const db = require('./db.js')
 const port = 3000
 
-app.get("/api", async (req,res) => {
-  console.log('hest');
-  var competitions = await db.findAllCompetitions();
-  res.send(competitions);
+app.use(bodyParser.json())
+
+const checkUser = function (req,res,next) {
+  if(req.headers.username && req.headers.password) {
+    db.findUserByUsernameAndPassword(req.headers.username, req.headers.password, (user) => {
+      if(user) {
+        req.user = user;
+        next();
+      } else {
+        res.send(401, 'user does not exist');
+      }
+      
+    })
+  } else {
+    res.send(400, 'missing authorization header');
+  }
   
-});
+}
+
+app.get("/course/list", async (req,res) => {
+  console.log('hest', req);
+  res.send({});
+  
+})
+
+app.post("/course", checkUser, async (req,res) => {
+  console.log('hest', req);
+  res.send(req.user);
+  
+})
 
 app.listen(port, () => { console.log(`running on port ${port}`)})
 
